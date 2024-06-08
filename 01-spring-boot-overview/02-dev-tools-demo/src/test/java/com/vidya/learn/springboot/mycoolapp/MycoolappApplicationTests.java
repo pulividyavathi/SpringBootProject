44ca58c251d0dac873157c2b580c0358716169e9
Vidyavathi Puli <puli.v@northeastern.edu>
2024-06-08 11:39:46 -0400

    Spring Project Setup

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/test/java/com/vidya/learn/springboot/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidya.learn.springboot.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
