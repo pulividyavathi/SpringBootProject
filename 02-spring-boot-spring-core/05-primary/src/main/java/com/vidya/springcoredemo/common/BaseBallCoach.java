e132ff08675d228b5f32368162270d59283426da
Vidyavathi Puli <puli.v@northeastern.edu>
2024-06-10 12:55:50 -0400

    Qualifiers

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/vidya/springcoredemo/common/BaseBallCoach.java
@@ -0,0 +1,12 @@
+package com.vidya.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseBallCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting practice baseball";
+    }
+}
---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/vidya/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.vidya.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 6k";
+    }
+}
