45b5ce7fb0894adaed0ec91fb5354dee5f968141
Vidyavathi Puli <puli.v@northeastern.edu>
2024-06-10 11:13:14 -0400

    Setter Injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/vidya/springcoredemo/rest/DemoController.java
@@ -0,0 +1,29 @@
+package com.vidya.springcoredemo.rest;
+
+import com.vidya.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+//    define a private field for the dependency
+    private Coach mycoach;
+
+////    define a constructor for dependency injection
+//    public DemoController(Coach theCoach){
+//         mycoach=theCoach;
+//    }
+
+    @Autowired
+    //Setter injection
+    public void setCoach(Coach theCoach){
+        mycoach=theCoach;
+    }
+@GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+      return mycoach.getDailyWorkout();
+}
+
+
+}
