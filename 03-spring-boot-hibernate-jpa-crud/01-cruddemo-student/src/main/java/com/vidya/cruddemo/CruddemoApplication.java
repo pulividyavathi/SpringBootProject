8ae9bd61a1df0ebc2eccb6318139a442a7d17e84
Vidyavathi Puli <puli.v@northeastern.edu>
2024-06-12 16:23:22 -0400

    Retrieve an Object with JPA

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/vidya/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/vidya/cruddemo/CruddemoApplication.java
@@ -17,9 +17,33 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner->{
 //			createStudent(studentDAO);
-			createMutlipleStudents(studentDAO);
+//			createMutlipleStudents(studentDAO);
+			readStudent(studentDAO);
+
 		};
   }
+
+	private void readStudent(StudentDAO studentDAO) {
+//		create a student object
+
+		System.out.println("Creating new student object");
+		Student tempStudent=new Student( "vikram","Puli","vikram@gmail.com");
+
+
+//		save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+//		display id of the saved student
+		int theId=tempStudent.getId();
+		System.out.println("Saved student. Generated id: "+theId);
+
+//		retrieve student based on the id: primary key
+		System.out.println("Retrieving the student with id: "+theId);
+		Student myStudent=studentDAO.findById(theId);
+//		display the student
+		System.out.println("Found the student: "+myStudent);
+	}
 
 	private void createMutlipleStudents(StudentDAO studentDAO) {
 //		create multiple student objects
