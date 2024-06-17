d024e29b0c01205d4cc272bfc09e82d6ed316cb2
Vidyavathi Puli <puli.v@northeastern.edu>
2024-06-17 12:15:59 -0400

    REST CRUD APIs Completed

---
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/vidya/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.vidya.springboot.cruddemo.dao;
+
+import com.vidya.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+//    that's it .. no need to write any code
+}
