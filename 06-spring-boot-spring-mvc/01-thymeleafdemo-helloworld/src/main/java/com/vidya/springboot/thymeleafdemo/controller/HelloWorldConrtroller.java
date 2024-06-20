060b35f300559f4a5fa3811a866d9999b48913c2
Vidyavathi Puli <puli.v@northeastern.edu>
2024-06-20 16:30:12 -0400

    Spring MVC Form Data Binding-Text Fields Completed

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/vidya/springboot/thymeleafdemo/controller/HelloWorldConrtroller.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/vidya/springboot/thymeleafdemo/controller/HelloWorldConrtroller.java
@@ -3,12 +3,15 @@ package com.vidya.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldConrtroller {
 //    neead a controller method to show intial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping ("/showForm")
 public String showForm(){
     return "helloworld-form";
 }
@@ -34,6 +37,24 @@ public String processForm(){
 //    add teh message to the model
 
     model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+
+
+
+    //need a controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+//    convert the data to all caps
+        theName=theName.toUpperCase();
+
+//    create the message
+        String result="Hey my friend from V3! "+theName;
+//    add teh message to the model
+
+        model.addAttribute("message",result);
 
         return "helloworld";
     }
---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/vidya/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.vidya.springboot.thymeleafdemo.controller;
+
+import com.vidya.springboot.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel){
+//        create a new student object
+        Student theStudent=new Student();
+//        add that student object to the model
+        theModel.addAttribute("student",theStudent);
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+//        log the input data
+        System.out.println("theStudent "+theStudent.getFirstName()+' '+theStudent.getLastName());
+        return "student-confirmation";
+    }
+}
