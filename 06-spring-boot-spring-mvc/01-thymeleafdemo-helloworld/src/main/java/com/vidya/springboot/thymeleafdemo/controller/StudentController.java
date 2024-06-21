187a257c0fc26bc1a81af09d1ace430928196148
Vidyavathi Puli <puli.v@northeastern.edu>
2024-06-21 12:47:38 -0400

    Thymeleaf checkbox,radio,list,dropdown features implemented

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/vidya/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/vidya/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,20 +1,41 @@
 package com.vidya.springboot.thymeleafdemo.controller;
 
 import com.vidya.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
+  @Value("${languages}")
+    private List<String> languages;
+
+
+  @Value("${systems}")
+  private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 //        create a new student object
         Student theStudent=new Student();
 //        add that student object to the model
         theModel.addAttribute("student",theStudent);
+
+//        add the list of countries to the model
+        theModel.addAttribute("countries",countries);
+
+//        add languages to the model
+        theModel.addAttribute("languages",languages);
+
+// add list of systems to the model
+        theModel.addAttribute("systems",systems);
         return "student-form";
     }
 
@@ -24,4 +45,6 @@ public class StudentController {
         System.out.println("theStudent "+theStudent.getFirstName()+' '+theStudent.getLastName());
         return "student-confirmation";
     }
+
+
 }
