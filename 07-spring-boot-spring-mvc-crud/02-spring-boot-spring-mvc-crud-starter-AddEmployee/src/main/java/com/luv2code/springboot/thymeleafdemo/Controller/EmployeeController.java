b26b17fbe0c08da760a8f04d24824176db858db5
Vidyavathi Puli <puli.v@northeastern.edu>
2024-07-02 15:50:23 -0400

    Update complete

--- 07-spring-boot-spring-mvc-crud/02-spring-boot-spring-mvc-crud-starter-AddEmployee/src/main/java/com/luv2code/springboot/thymeleafdemo/Controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/02-spring-boot-spring-mvc-crud-starter-AddEmployee/src/main/java/com/luv2code/springboot/thymeleafdemo/Controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,6 +36,17 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId,Model theModel){
+//        get the employee from the service
+        Employee theEmployee=employeeService.findById(theId);
+//        set employee in the model to prepopulate the form
+        theModel.addAttribute("employee",theEmployee);
+
+//        send over to our form to display
+        return "employees/employee-form";
+    }
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
 //        save the employee
@@ -46,4 +54,6 @@ public class EmployeeController {
 //        use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
     }
+
+
 }
