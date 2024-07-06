7877c45e6e1f31ac684ac21451fcabb68e9ac0b3
Vidyavathi Puli <puli.v@northeastern.edu>
2024-07-06 16:58:52 -0400

    Custom Login form using Spring Security

---
+++ 08-springboot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/vidya/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.vidya.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showLoginForm(){
+        return "fancy-login";
+    }
+}
