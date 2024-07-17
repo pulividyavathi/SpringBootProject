77abd5fedf95e5ef1deb6cfd643c489c69b60072
Vidyavathi Puli <puli.v@northeastern.edu>
2024-07-17 14:31:07 -0400

    @Around Advice Rethrow Exception

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/vidya/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/vidya/aopdemo/AopdemoApplication.java
@@ -31,10 +31,21 @@ public class AopdemoApplication {
 //			demoTheAfterThrowingAdvice(theAccountDAO);
 //			demoTheAfterAdvice(theAccountDAO);
 //			demoTheAroundAdvice(theTrafficFortuneService);
-			demoTheAroundAdviceHandleException(theTrafficFortuneService);
+//			demoTheAroundAdviceHandleException(theTrafficFortuneService);
+			demoTheAroundAdviceRethrowException(theTrafficFortuneService);
 
 
 		};
+	}
+
+	private void demoTheAroundAdviceRethrowException(TrafficFortuneService theTrafficFortuneService) {
+		System.out.println("\nMain Program: demoTheAroundAdviceRethrowException");
+		System.out.println("Calling getFortune()");
+
+		boolean tripWire=true;
+		String data=theTrafficFortuneService.getFortune(tripWire);
+		System.out.println("\nMy Fortune is "+data);
+		System.out.println("Finished");
 	}
 
 	private void demoTheAroundAdviceHandleException(TrafficFortuneService theTrafficFortuneService) {
